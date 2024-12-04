591cdaeb123148080668b64efb71c8cc2166fff3
Arif Khan <skjobbd276@gmail.com>
2024-12-04 20:19:10 +0600

    Shortest job first

--- test.cpp
+++ test.cpp
@@ -1,20 +1,52 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+#include<cctype>
+#include<math.h>
+#include<cmath>
+#include<vector>
 using namespace std;
-
+#define ll long long
+ 
 int main() {
-	int t;
-	cin>>t;
 	
-	while(t--){
-	    ll n,x;
-	    cin>>n>>x;
-	    string ans="YES";
-	    
-	    if(x%2==0 && (n%2)!=0)
-	        ans="NO";
-	        
-	   cout<<ans<<endl;
+	/*
+         █████    ███████    ██   █████████
+        ██╔══██╗  ██    ██   ██   ██
+        ███████║  ███████    ██   ███████
+        ██╔══██║  ██ ██      ██   ██
+        ██║  ██║  ██   ██    ██   ██
+        ╚═╝  ╚═╝  
+	  
+	*/
+    int bt[4]={3,4,2,4};
+	int btt[4]={3,4,2,4};
+	int ct[4];
+	sort(bt, bt+4);
+	int sum=0;
+	
+	for(int i=0; i<3; i++)
+	{
+		
+		for(int j=0; j<4; j++)   
+		{
+			
+			if(bt[i]==btt[j])
+			{
+				sum+=btt[j];
+				cout<<sum<<" "<<j+1<<" " <<i+1<<endl;
+				
+
+				ct[j]=sum;
+				
+
+			}
+		}
 	}
-	return 0;
+	for(int i=0 ; i<4; i++)
+	{
+		cout<<ct[i]<<" ";
+	}
+
+    return 0;
 }
